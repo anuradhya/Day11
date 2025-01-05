5d5b355230c8c1ac9797512fa3d8b28ea3a13216
Anuradhya Liyanaarachchi <139705037+anuradhya@users.noreply.github.com>
2025-01-05 06:26:27 +0000

    Updated Home.js css

--- navigationapp/components/Home.js
+++ navigationapp/components/Home.js
@@ -34,3 +34,19 @@ export default function Home() {
     </PaperProvider>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        backgroundColor: '#fff',
+        justifyContent: 'space-between',
+        padding: 10
+    },
+    body: {
+        padding: 8,
+        textAlign: 'justify'
+    },
+    scrollView: {
+        flexGrow: 1, 
+    },
+});
